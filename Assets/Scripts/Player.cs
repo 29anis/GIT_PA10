d1c6f928368a8d77db9750facbf849e298023ddf
29anis <155930894+29anis@users.noreply.github.com>
2024-01-23 17:34:50 +0800

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class Player : MonoBehaviour
 {
+
+    int force = 200;
+    public GameObject obstacle;
+    public Text scoretext;
+    public float speed;
+
     private Animation thisAnimation;
 
     void Start()
@@ -15,6 +24,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, force, 0));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "obstacle")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
     }
 }
---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreScript : MonoBehaviour
+{
+
+    int score;
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    if (collision.gameObject.tag=="obstacle")
+    {
+        score++;
+        ScoreText.text = "Score :" + score;
+
+
+        Destroy(collision.gameObject);
+        }
+    }
+}
